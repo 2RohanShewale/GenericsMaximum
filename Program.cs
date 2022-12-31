5bd35cd0afc998dd831ea9320262fa9247e00542
2RohanShewale <shewalerohan50@gmail.com>
2022-12-31 08:10:20 +0530

    TS1.3

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     static void Main(string[] args)
     {
-        int max = FindMaximum.FindIntMax(2595,4595, 4002);
+        int max = FindMaximum.FindIntMax(2595,3587, 4595);
         Console.WriteLine(max);
     }
 }
