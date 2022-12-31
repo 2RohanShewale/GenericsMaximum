cdf100803be7c0746e36ce2a9944b7f09ac0db33
2RohanShewale <shewalerohan50@gmail.com>
2022-12-31 08:04:33 +0530

    TS1.1

---
+++ FindIntMax.cs
@@ -0,0 +1,19 @@
+﻿public class FindMaximum
+{
+    public static int FindIntMax(int num1, int num2, int num3)
+    {
+        if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+        {
+            return num1;
+        }
+        if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+        {
+            return num2;
+        }
+        if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+        {
+            return num3;
+        }
+        return 0;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        int max = FindMaximum.FindIntMax(4595, 2332, 4002);
+        Console.WriteLine(max);
+    }
+}
