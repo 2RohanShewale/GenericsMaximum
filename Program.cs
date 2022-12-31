f3c73694e13b6cca8eaf03de24db9b9bbb76edc3
2RohanShewale <shewalerohan50@gmail.com>
2022-12-31 08:14:31 +0530

    TS1.1_Float

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     static void Main(string[] args)
     {
-        int max = FindMaximum.FindIntMax(2595,3587, 4595);
-        Console.WriteLine(max);
+        float floatmax = FindMaximum.FindfloatMax(422.25f, 256.23f, 388.23f);
+        Console.WriteLine(floatmax);
     }
 }
