e704bfa3ee7915e9d4f6e5fa7a73cabadb487579
2RohanShewale <shewalerohan50@gmail.com>
2022-12-31 08:52:20 +0530

    Refractor

--- FindIntMax.cs
+++ FindIntMax.cs
@@ -1,45 +1,23 @@
 ﻿public class FindMaximum
 {
-    public static int FindIntMax(int num1, int num2, int num3)
-    {
-        if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+ 
+        public static T FindMaximumNumber<T>(T num1, T num2, T num3) where T : IComparable
         {
-            return num1;
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+            {
+                return num3;
+            }
+            return default;
         }
-        if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
-        {
-            return num2;
-        }
-        if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
-        {
-            return num3;
-        }
-        return 0;
-    }
-    public static float FindfloatMax(float num1, float num2, float num3)
-    {
-        if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
-        {
-            return num1;
-        }
-        if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
-        {
-            return num2;
-        }
-        if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
-        {
-            return num3;
-        }
-        return 0;
-    }
-    public static string FindStringMax(string myString1, string myString2, string myString3)
-    {
-        int max = 0;
-        string s = "";
-        if (myString1.Length > max) { max = myString1.Length; s = myString1; }
-        if (myString2.Length > max) { max = myString2.Length; s = myString2; }
-        if (myString3.Length > max) { max = myString3.Length; s = myString3; }
-        return s;
-    }
+
+
 
 }
--- Program.cs
+++ Program.cs
@@ -2,7 +2,11 @@
 {
     static void Main(string[] args)
     {
-        string stringMax = FindMaximum.FindStringMax("Apple", "Peach", "Banana");
+        int max = FindMaximum.FindMaximumNumber<int>(2332, 4002, 4595);
+        Console.WriteLine(max);
+        float floatmax = FindMaximum.FindMaximumNumber<float>(256.23f, 388.23f, 422.25f);
+        Console.WriteLine(floatmax);
+        string stringMax = FindMaximum.FindMaximumNumber<string>("Apples", "Peach", "Banana");
         Console.WriteLine(stringMax);
     }
 }
